be1b485facd684cd49d0ede8aa0a99fa73414e7a
KristinaAD <kriwowak@yandex.ru>
2023-09-28 00:37:39 +0500

    first commit

---
+++ src/main/java/com/gridnine/testing/TestingFilterApplication.java
@@ -0,0 +1,13 @@
+package com.gridnine.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingFilterApplication.class, args);
+    }
+
+}
